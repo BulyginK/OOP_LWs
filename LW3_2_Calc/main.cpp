b07cef509d3a821930a9d4af1289fa0fa758faad
BulyginK <bulygin.k@gmail.com>
2025-03-26 17:45:56 +0300

    correct

--- LW3_2_Calc/main.cpp
+++ LW3_2_Calc/main.cpp
@@ -4,15 +4,6 @@
 int main()
 {
 	CCalculator calc;
-	//CCalcMenu control—alculator(calc, cin, cout);
-	//
-	//while (!std::cin.eof() && !std::cin.fail())
-	//{
-	//	std::cout << "> ";
-	//	if (!control—alculator.RunCommand())
-	//	{
-	//		std::cout << "Invalid command!" << std::endl;
-	//	}
-	//}
+
 	return 0;
 }
